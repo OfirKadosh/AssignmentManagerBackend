f454768b94f06af62dadaf4bf92638f73717a133
Ofir Kadosh <ofir.kadoshosh@gmail.com>
2021-12-17 18:37:24 +0200

    moving to PC from laptop.

---
+++ Backend/BackendHost/Controllers/AllUsersController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendHost.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AllUsersController : ControllerBase
+    {
+        IUserRepository _repo;
+        public AllUsersController(IUserRepository userRepository) { _repo = userRepository; }
+
+        // GET <AllUsersController>
+        [HttpGet]
+        public ICollection<User> Get() => _repo.GetUsers();
+    }
+}
